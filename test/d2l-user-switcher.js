0db3ac812466f8f93c674cf49e8cba7c3b4dd79b
Colin Stranc <Colin.Stranc@D2L.com>
2017-07-24 11:47:38 -0500

    added more tests

--- test/d2l-user-switcher.js
+++ test/d2l-user-switcher.js
@@ -16,6 +16,54 @@ describe('d2l-user-switcher', function() {
 		});
 	});
 
+	describe('Parent with no children', function() {
+		var parentData = { entities: [] };
+
+		beforeEach(function() {
+			component.parentData = parentData;
+		});
+
+		describe('user switcher', function() {
+			it('should not have an opener container', function(done) {
+				setTimeout(function() {
+					var shouldNotExist = component.querySelector('.d2l-user-switcher-opener-container');
+					expect(shouldNotExist).to.not.exist;
+					done();
+				});
+			});
+		});
+
+		describe('switch parent data', function() {
+			describe('go to 1 child', function() {
+				var newParentData = { entities: [{}] };
+
+				it('now displays content', function(done) {
+					component.parentData = newParentData;
+
+					setTimeout(function() {
+						var container = component.querySelector('.d2l-user-switcher-opener-container');
+						expect(container).to.exist;
+						done();
+					});
+				});
+			});
+
+			describe('go to 2 children', function() {
+				var newParentData = { entities: [{}, {}] };
+
+				it('now has dropdown', function(done) {
+					component.parentData = newParentData;
+
+					setTimeout(function() {
+						var dropdown = component.querySelector('d2l-dropdown');
+						expect(dropdown).to.exist;
+						done();
+					});
+				});
+			});
+		});
+	});
+
 	describe('Parent with one child', function() {
 		var parentData = { entities: [{}] };
 
@@ -29,6 +77,36 @@ describe('d2l-user-switcher', function() {
 					var dropdown = component.querySelector('d2l-dropdown');
 					expect(dropdown).to.not.exist;
 					done();
+				});
+			});
+		});
+
+		describe('switch parent data', function() {
+			describe('go to 2 children', function() {
+				var newParentData = { entities: [{}, {}] };
+
+				it('now displays dropdown', function(done) {
+					component.parentData = newParentData;
+
+					setTimeout(function() {
+						var dropdown = component.querySelector('d2l-dropdown');
+						expect(dropdown).to.exist;
+						done();
+					});
+				});
+			});
+
+			describe('go to 0 child', function() {
+				var newParentData = { entities: [] };
+
+				it('no longer has content', function(done) {
+					component.parentData = newParentData;
+
+					setTimeout(function() {
+						var shouldNotExist = component.querySelector('.d2l-user-switcher-opener-container');
+						expect(shouldNotExist).to.not.exist;
+						done();
+					});
 				});
 			});
 		});
@@ -119,14 +197,100 @@ describe('d2l-user-switcher', function() {
 			});
 		});
 
-		describe('drop down should contain', function() {
-			it('an entry for each child', function(done) {
+		describe('switch parent data', function() {
+			describe('go to 1 child', function() {
+				var newParentData = { entities: [{}] };
+
+				it('no longer displays dropdown', function(done) {
+					component.parentData = newParentData;
+
+					setTimeout(function() {
+						var dropdown = component.querySelector('d2l-dropdown');
+						expect(dropdown).to.not.exist;
+						done();
+					});
+				});
+			});
+
+			describe('go to 0 child', function() {
+				var newParentData = { entities: [] };
+
+				it('no longer has content', function(done) {
+					component.parentData = newParentData;
+
+					setTimeout(function() {
+						var shouldNotExist = component.querySelector('.d2l-user-switcher-opener-container');
+						expect(shouldNotExist).to.not.exist;
+						done();
+					});
+				});
+			});
+		});
+
+		describe('drop down', function() {
+			it('should contain an entry for each child', function(done) {
 				setTimeout(function() {
 					var dropdown = component.querySelector('d2l-dropdown');
 					var entries = Polymer.dom(dropdown).querySelectorAll('d2l-user-switcher-item');
 
 					expect(entries.length).to.be.equal(parentData.entities.length);
 					done();
+				});
+			});
+
+			it('should fire event when an item is clicked', function(done) {
+				setTimeout(function() {
+					var dropdown = component.querySelector('d2l-dropdown');
+					var entries = Polymer.dom(dropdown).querySelectorAll('d2l-user-switcher-item');
+
+					var studentSelectedSpy = sinon.spy();
+
+					setTimeout(function() {
+						expect(studentSelectedSpy).to.be.called;
+						done();
+					});
+
+					component.addEventListener('studentSelected', studentSelectedSpy);
+					var event = new Event('tap');
+					entries[0].dispatchEvent(event);
+				});
+			});
+
+			it('should fire event when enter is pressed on item', function(done) {
+				setTimeout(function() {
+					var dropdown = component.querySelector('d2l-dropdown');
+					var entries = Polymer.dom(dropdown).querySelectorAll('d2l-user-switcher-item');
+
+					var studentSelectedSpy = sinon.spy();
+
+					setTimeout(function() {
+						expect(studentSelectedSpy).to.be.called;
+						done();
+					});
+
+					component.addEventListener('studentSelected', studentSelectedSpy);
+					var event = new Event('keydown');
+					event.keyCode = 13;
+					entries[0].dispatchEvent(event);
+				});
+			});
+
+			it('should fire event when space is pressed on item', function(done) {
+				setTimeout(function() {
+					var dropdown = component.querySelector('d2l-dropdown');
+					var entries = Polymer.dom(dropdown).querySelectorAll('d2l-user-switcher-item');
+
+					var studentSelectedSpy = sinon.spy();
+
+					setTimeout(function() {
+						expect(studentSelectedSpy).to.be.called;
+						done();
+					});
+
+					component.addEventListener('studentSelected', studentSelectedSpy);
+					var event = new Event('keydown');
+					event.keyCode = 32;
+					entries[0].dispatchEvent(event);
 				});
 			});
 		});
